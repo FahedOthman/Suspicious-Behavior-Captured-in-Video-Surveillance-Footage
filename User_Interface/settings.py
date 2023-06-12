38152baae5a6feb97216c5f426e8291654d07d9e
FahedOthman <136342616+FahedOthman@users.noreply.github.com>
2023-06-12 16:53:39 +0300

    Add files via upload

---
+++ User_Interface/settings.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import sys
+import tensorflow as tf
+
+# Get the absolute path of the current file
+file_path = Path(__file__).resolve()
+
+# Get the parent directory of the current file
+root_path = file_path.parent
+
+# Add the root path to the sys.path list if it is not already there
+if root_path not in sys.path:
+    sys.path.append(str(root_path))
+
+# Get the relative path of the root directory with respect to the current working directory
+ROOT = root_path.relative_to(Path.cwd())
+
+# Sources
+IMAGE = 'Image'
+VIDEO = 'Video'
+WEBCAM = 'Webcam'
+RTSP = 'RTSP'
+YOUTUBE = 'YouTube'
+
+SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
+
+# Images config
+IMAGES_DIR = ROOT / 'images'
+DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+
+# Videos config
+VIDEO_DIR = ROOT / 'videos'
+VIDEO_1_PATH = VIDEO_DIR / 'video1.mp4'
+VIDEO_2_PATH = VIDEO_DIR / 'video2.mp4'
+VIDEO_3_PATH = VIDEO_DIR / 'video3.mp4'
+VIDEOS_DICT = {
+    'video1.mp4': VIDEO_1_PATH,
+    'video2.mp4': VIDEO_2_PATH,
+    'video3.mp4': VIDEO_3_PATH,
+}
+
+# ML Model config
+MODEL_DIR = ROOT / 'weights'
+DETECTION_MODEL = MODEL_DIR / 'yolov7 (2).pt'
+SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg (1).pt'
+# Webcam
+WEBCAM_PATH = 0
